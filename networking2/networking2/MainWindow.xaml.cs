c5ffb84f76373c41caec9d93d714f542768de96d
Tenille <tenille_brathwaite@hotmail.com>
2013-02-04 12:05:27 -0500

    commit this stuff

---
+++ networking2/networking2/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+
+namespace networking2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+       internal static string c_username, c_password, error, file_loc = string.Empty;
+        internal static string address = string.Empty;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+       private bool isValidConnection(string url, string user, string password)
+             {
+           
+                 try 
+            {
+                
+                     FtpWebRequest requestDir = (FtpWebRequest)WebRequest.Create("ftp://"+address);
+                requestDir.Method = WebRequestMethods.Ftp.ListDirectory;
+                requestDir.Credentials = new NetworkCredential(c_username, c_password);
+                WebResponse response = requestDir.GetResponse();
+                
+            }
+            catch
+            {
+                error = textBox1.Text = "failed";
+    return false;
+            }
+                 error = textBox1.Text = "Connection Established";
+return true;
+       }
+        private void submit_Click(object sender, RoutedEventArgs e)
+        {
+            address = server.Text = "drwestfall.info";
+            c_username = username.Text = "project01";
+            c_password = pword.Text = "csci355";
+            StringBuilder result = new StringBuilder();
+            //isValidConnection(server.Text,username.Text,password.Text);
+
+
+            
+           FtpWebRequest requestDir = (FtpWebRequest)WebRequest.Create("ftp://"+address);
+            requestDir.Method = WebRequestMethods.Ftp.ListDirectory;
+            requestDir.Credentials = new NetworkCredential(c_username, c_password);
+            WebResponse response = requestDir.GetResponse();
+             FtpWebResponse responseDir = (FtpWebResponse)requestDir.GetResponse();
+             StreamReader readerDir = new StreamReader(responseDir.GetResponseStream()); 
+             //error = "Connected to the server";
+             Upload w2 = new Upload();
+            w2.Show();
+           // this.Hide();
+           
+           
+         }
+
+       
+}
+    }
---
+++ networking2/networking2/Upload.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using System.Net;
+
+namespace networking2
+{
+    /// <summary>
+    /// Interaction logic for Upload.xaml
+    /// </summary>
+    public partial class Upload : Window 
+    {
+        public Upload()
+        {
+            InitializeComponent();
+          // MessageBox.Show(MainWindow.
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+           
+           // Uri target = new Uri("ftp"+);
+            string fileName = "c:\\test2.txt";
+
+           // if (File.Exists(fileName))
+
+
+               // MessageBox.Show(MainWindow.error);
+
+          //  else
+
+               // MessageBox.Show(MainWindow.error);
+
+           // MessageBox.Show(MainWindow.address);
+            FileInfo toUpload = new FileInfo(fileName);
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://" + MainWindow.address + "/" + toUpload.Name);
+
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.Credentials = new NetworkCredential(MainWindow.c_username, MainWindow.c_password);
+            Stream ftpStream = request.GetRequestStream();
+            FileStream file = File.OpenRead(fileName);
+            int length = 1024;
+            byte[] buffer = new byte[length];
+            int bytesRead = 0;
+
+            do
+            {
+                bytesRead = file.Read(buffer, 0, length);
+                ftpStream.Write(buffer, 0, bytesRead);
+            }
+            while (bytesRead != 0);
+
+            file.Close();
+            ftpStream.Close();
+
+            MessageBox.Show("Upload Complete");
+
+        }
+
+        private void button1_Click_1(object sender, RoutedEventArgs e)
+        {
+            WebClient request = new WebClient();
+            request.Credentials = new NetworkCredential(MainWindow.c_username, MainWindow.c_password);
+            byte[] fileData = request.DownloadData("ftp://" + MainWindow.address + "/" + "test2.txt");
+            FileStream file = File.Create("c:\\yada\\" + "test2.txt");
+            file.Write(fileData, 0, fileData.Length);
+            file.Close();
+            MessageBox.Show("Download complete");
+
+        }
+    }
+}
