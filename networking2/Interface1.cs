fbea154aa0f66bf61d29e3763aa8dcdee2531279
Tenille <tenille_brathwaite@hotmail.com>
2013-02-25 11:55:34 -0500

    progress bar partially working

---
+++ networking2/Interface1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace networking2
+{
+    interface IProgressContext
+    {
+        void UpdateProgress(double progress);
+        void UpdateStatus(string status);
+        void Finish();
+        bool Canceled { get; }
+    }
+}
--- networking2/Upload.xaml.cs
+++ networking2/Upload.xaml.cs
@@ -12,31 +12,63 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.IO;
 using System.Net;
+using System.ComponentModel;
+using System.Windows.Threading;
+using System.Threading;
 
 namespace networking2
 {
     /// <summary>
     /// Interaction logic for Upload.xaml
     /// </summary>
-    public partial class Upload : Window 
+    
+  //public delegate void ProgressWorkerDelegate(IProgressContext progressContext);
+
+    public partial class Upload : Window, IProgressContext
     {
-        internal static string upload_file; 
+        internal static string upload_file;
+
+        private bool canceled = false;
+     //   private ProgressWorkerDelegate workDelegate = null;
+
+        public bool Canceled
+        {
+            get { return canceled; }
+        }
+
         public Upload()
         {
             InitializeComponent();
-            uploadFile.Text = upload_file; 
+            uploadFile.Text = upload_file;
+
+            CancelButton.Click += new RoutedEventHandler(CancelButton_Click);
+        }
+
+
+        public void UpdateProgress(double progress)
+        {
+            Dispatcher.BeginInvoke(DispatcherPriority.Background,
+                (SendOrPostCallback)delegate { Progress.SetValue(ProgressBar.ValueProperty, progress); }, null);
+        }
+
+        public void UpdateStatus(string status)
+        {
+            Dispatcher.BeginInvoke(DispatcherPriority.Background,
+                (SendOrPostCallback)delegate { StatusText.SetValue(TextBlock.TextProperty, status); }, null);
+        }
+
+        public void Finish()
+        {
+            Dispatcher.BeginInvoke(DispatcherPriority.Background,
+                (SendOrPostCallback)delegate { Close(); }, null);
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
+            
+          FileInfo toUpload = new FileInfo(uploadFile.Text);
 
-
-
-
-            FileInfo toUpload = new FileInfo(uploadFile.Text);
-
-            Console.WriteLine("These are the current values: " + MainWindow.c_username + MainWindow.c_password + MainWindow.address);
-           
+            
             FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://" + MainWindow.address + "/" + toUpload.Name);
 
             request.Method = WebRequestMethods.Ftp.UploadFile;
@@ -46,31 +78,57 @@ namespace networking2
             int length = 1024;
             byte[] buffer = new byte[length];
             int bytesRead = 0;
+            
+            double divide = 1;
 
+            long filesize = toUpload.Length;
+
+/*            if (filesize < 100000)
+                divide = 1;
+            if (filesize > 100000)
+                divide = 0.001;
+
+            for (int i = 0; i < filesize; i++)
+                {
+                    if (this.Canceled)
+                        break;
+
+                    this.UpdateProgress((double)i/divide);
+                    this.UpdateStatus("Doing Step " + i);
+                }
+  */
+            int totalReadBytesCount = 0;
+            double progress;
             do
             {
                 bytesRead = file.Read(buffer, 0, length);
+                
+                totalReadBytesCount += bytesRead;
+                progress = totalReadBytesCount * 100.0 / toUpload.Length;
+
+                this.UpdateProgress((double) progress);
+                this.UpdateStatus("Doing Step " + progress);
                 ftpStream.Write(buffer, 0, bytesRead);
-            }
+             }
             while (bytesRead != 0);
 
             file.Close();
             ftpStream.Close();
 
+            this.Finish();
+
             MessageBox.Show("Upload Complete");
-
         }
 
-        private void button1_Click_1(object sender, RoutedEventArgs e)
+           
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
-            WebClient request = new WebClient();
-            request.Credentials = new NetworkCredential(MainWindow.c_username, MainWindow.c_password);
-            byte[] fileData = request.DownloadData("ftp://" + MainWindow.address + "/" + "test2.txt");
-            FileStream file = File.Create("c:\\yada\\" + "test2.txt");
-            file.Write(fileData, 0, fileData.Length);
-            file.Close();
-            MessageBox.Show("Download complete");
-
+            canceled = true;
+            CancelButton.IsEnabled = false;
         }
+
+
     }
+
 }
