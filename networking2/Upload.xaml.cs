a3b2010667a3af412ca8e38305222ccc559dd584
Tenille <tenille_brathwaite@hotmail.com>
2013-02-25 19:19:02 -0500

    upload progress bar 90% done  I just need to tweak it to reflect the size of the file hopefully that wont be too hard

--- networking2/Upload.xaml.cs
+++ networking2/Upload.xaml.cs
@@ -22,113 +22,90 @@ namespace networking2
     /// Interaction logic for Upload.xaml
     /// </summary>
     
-  //public delegate void ProgressWorkerDelegate(IProgressContext progressContext);
 
-    public partial class Upload : Window, IProgressContext
+
+    public partial class Upload : Window
     {
         internal static string upload_file;
-
+        public string file_upload;
         private bool canceled = false;
-     //   private ProgressWorkerDelegate workDelegate = null;
 
-        public bool Canceled
-        {
-            get { return canceled; }
-        }
+
+        BackgroundWorker backgroundWorker = new BackgroundWorker();
 
         public Upload()
         {
             InitializeComponent();
             uploadFile.Text = upload_file;
 
-            CancelButton.Click += new RoutedEventHandler(CancelButton_Click);
-        }
-
-
-        public void UpdateProgress(double progress)
-        {
-            Dispatcher.BeginInvoke(DispatcherPriority.Background,
-                (SendOrPostCallback)delegate { Progress.SetValue(ProgressBar.ValueProperty, progress); }, null);
-        }
-
-        public void UpdateStatus(string status)
-        {
-            Dispatcher.BeginInvoke(DispatcherPriority.Background,
-                (SendOrPostCallback)delegate { StatusText.SetValue(TextBlock.TextProperty, status); }, null);
-        }
-
-        public void Finish()
-        {
-            Dispatcher.BeginInvoke(DispatcherPriority.Background,
-                (SendOrPostCallback)delegate { Close(); }, null);
-        }
-
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.DoWork += new DoWorkEventHandler(backgroundWorker_DoWork);
+            backgroundWorker.ProgressChanged += new ProgressChangedEventHandler(backgroundWorker_ProgressChanged);
             
-          FileInfo toUpload = new FileInfo(uploadFile.Text);
+        }
 
-            
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            file_upload = upload_file;
+        }
+
+
+
+        void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Progress.Value = e.ProgressPercentage;
+        }
+
+        void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            FileInfo toUpload = new FileInfo(file_upload);
+
+
+
             FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://" + MainWindow.address + "/" + toUpload.Name);
 
             request.Method = WebRequestMethods.Ftp.UploadFile;
             request.Credentials = new NetworkCredential(MainWindow.c_username, MainWindow.c_password);
             Stream ftpStream = request.GetRequestStream();
-            FileStream file = File.OpenRead(uploadFile.Text);
+            FileStream file = File.OpenRead(file_upload);
             int length = 1024;
             byte[] buffer = new byte[length];
             int bytesRead = 0;
-            
-            double divide = 1;
 
             long filesize = toUpload.Length;
 
-/*            if (filesize < 100000)
-                divide = 1;
-            if (filesize > 100000)
-                divide = 0.001;
 
-            for (int i = 0; i < filesize; i++)
-                {
-                    if (this.Canceled)
-                        break;
 
-                    this.UpdateProgress((double)i/divide);
-                    this.UpdateStatus("Doing Step " + i);
-                }
-  */
             int totalReadBytesCount = 0;
             double progress;
             do
             {
                 bytesRead = file.Read(buffer, 0, length);
-                
+
                 totalReadBytesCount += bytesRead;
                 progress = totalReadBytesCount * 100.0 / toUpload.Length;
-
-                this.UpdateProgress((double) progress);
-                this.UpdateStatus("Doing Step " + progress);
+                backgroundWorker.ReportProgress((int)progress);
                 ftpStream.Write(buffer, 0, bytesRead);
-             }
+            }
             while (bytesRead != 0);
 
             file.Close();
             ftpStream.Close();
 
-            this.Finish();
-
             MessageBox.Show("Upload Complete");
         }
 
-           
 
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
+
+        private void button1_Click(object sender, RoutedEventArgs e)
         {
-            canceled = true;
-            CancelButton.IsEnabled = false;
+
+            backgroundWorker.RunWorkerAsync();
+
         }
 
-
+      
     }
 
 }
